6ca67b41269b3f40bb4992e07613ca2b55ffac46
rigolinux <rigoberto7567@gmail.com>
2024-01-17 09:59:09 -0600

    first steps with rust

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    
+    let x = 5;
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of x is: {}", x);
+    println!("Hello i am from docker!");
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+}
